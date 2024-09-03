ee2d2425637fcfa857814b611c17d88f7941efba
vishwanathguru <116871883+vishwanathguru@users.noreply.github.com>
2024-09-03 13:17:17 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// Primitive
+
+// tatal 7types: String, number, boolearn, null, undifined, symbol, BigInt
+
+// reference (Non premitive)
+
+// Array, Object, Function
+
+const heros = ["ironman", "captainamerica", "spiderman", "hulk", "thor"] 
+// [] this is Array 
+
+let myObj = {                   
+    name: "vishwanath",  
+    age: 32,
+
+}
+// {} this is Object
+
+
+const myFunction = function () {
+    console.log("Hello World");
+    
+}
+// this is Function(){}
+
+
+console.log(typeof myFunction);
