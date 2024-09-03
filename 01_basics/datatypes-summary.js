1abe21b4001e41c20cfa580a95b741e40c8621b4
vishwanathguru <116871883+vishwanathguru@users.noreply.github.com>
2024-09-03 13:54:58 +0000

    datatype-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,16 +12,42 @@ const heros = ["ironman", "captainamerica", "spiderman", "hulk", "thor"]
 let myObj = {                   
     name: "vishwanath",  
     age: 32,
-
 }
+
 // {} this is Object
 
 
-const myFunction = function () {
-    console.log("Hello World");
-    
-}
+// const myFunction = function () {
+    // console.log("Hello World");}
+
 // this is Function(){}
 
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++**++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// stack (primative),   Heap (non-primative)
+
+let myYoutubenam = "vishwanath@guru.com"
+let anotherNmae = myYoutubenam
+anotherNmae = "niranjan@guru.com"
+
+// console.log(myYoutubenam)
+//console.log(anotherNmae)
+
+let userOne = {
+    email: "vishwanath@guru.com",
+    upi: "yxz@yty"
+}
+
+let userTwo = userOne
+userTwo.email = "deepak@yadav.com"
+
+// console.log(userTwo);
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
